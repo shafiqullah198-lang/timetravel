3de182ee173af3142b6df0a9f5c10e9e53506730
shafiqullah198-lang <shafiqullah198@gmail.com>
2026-03-03 07:05:09 +0500

    Switch to MySQL for demo

--- travel_agency/settings.py
+++ travel_agency/settings.py
@@ -84,12 +84,8 @@ WSGI_APPLICATION = 'travel_agency.wsgi.application'
 # -------------------------------------------------
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'TIME TRAVEL AND TOURS',
-        'USER': 'postgres',
-        'PASSWORD': 'khan1',
-        'HOST': 'localhost',
-        'PORT': '5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
